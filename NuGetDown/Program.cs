a7b36e74619dbf967dfa66012018f3832bd0572f
aminic <aminic.null@gmail.com>
2015-12-25 13:52:21 +0800

    new function : call format 2 : NuGetDown -f packagefilename

--- NuGetDown/Program.cs
+++ NuGetDown/Program.cs
@@ -35,6 +35,7 @@ namespace NuGetDown
 
             this.packageName = packageName;
             packageUrl = string.Format(PackageUrlFormat, this.packageName);
+            Console.WriteLine("##Package Version Url : {0}", packageUrl);
         }
 
         public void GetAllVersion()
@@ -139,9 +140,9 @@ namespace NuGetDown
                 if (string.IsNullOrEmpty(message))
                     message = string.Format("{0} down OK.", version);
                 Console.WriteLine(message);
-                var r =new Random();
-                var time = r.Next(11,30);
-                System.Threading.Thread.Sleep(time*1000);
+                var r = new Random();
+                var time = r.Next(11, 30);
+                System.Threading.Thread.Sleep(time * 1000);
 
                 //////////////////////////////////
                 //multi thread down
@@ -191,29 +192,60 @@ namespace NuGetDown
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("call format: NuGetDown packagename nupkgTargetFolder");
+                Console.WriteLine("call format 1 : NuGetDown packagename nupkgTargetFolder");
+                Console.WriteLine("call format 2 : NuGetDown -f packagefilename");
                 return;
             }
 
             DirectoryInfo dir;
-            if (args.Length == 1)
-                dir = new DirectoryInfo(System.Environment.CurrentDirectory);
-            else
-                dir = new DirectoryInfo(args[1]);
-            if (!dir.Exists)
+
+            if (args[0] == "-f")
             {
-                Console.WriteLine("nupkg target folder is not exist");
+                Console.WriteLine("####down package from file");
+                if (args.Length != 2)
+                {
+                    Console.WriteLine("-f need valid filename");
+                    return;
+                }
+
+                var fileFullPath = new FileInfo(System.Environment.CurrentDirectory + "\\" + args[1]);
+                Console.WriteLine("package file full path : {0}", fileFullPath);
+                if (!fileFullPath.Exists)
+                {
+                    Console.WriteLine("packagefilename must exist");
+                    return;
+                }
+                dir = new DirectoryInfo(System.Environment.CurrentDirectory);
+                var packages = File.ReadAllLines(fileFullPath.FullName);
+                for (int i = 0; i < packages.Length; i++)
+                {
+                    var packageName = packages[i];
+                    Console.WriteLine("# down package : {0} {1}", i, packageName);
+                    var d = new DownNupkg(packageName, dir);
+                    d.GetAllVersion();
+                    d.Down();
+                }
                 return;
             }
-            var d = new DownNupkg(args[0], dir);
+            else
+            {
+                Console.WriteLine("####down one package");
+                if (args.Length == 1)
+                    dir = new DirectoryInfo(System.Environment.CurrentDirectory);
+                else
+                    dir = new DirectoryInfo(args[1]);
+                if (!dir.Exists)
+                {
+                    Console.WriteLine("nupkg target folder is not exist");
+                    return;
+                }
+                var d = new DownNupkg(args[0], dir);
 
-            d.GetAllVersion();
-            d.Down();
+                d.GetAllVersion();
+                d.Down();
+
+            }
             Console.WriteLine("#END#");
-            //Console.Read();
-
-
-
         }
     }
 
